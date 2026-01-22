4173d4b76ec9c9990077ce878cb284c4f7bd19ba
takuho01 <krein.to112@gmail.com>
2026-01-22 23:07:41 +0900

    add

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+
+
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  console.log('クリックされました！');
+  const text = document.getElementById('text');
+  text.textContent = 'ボタンをクリックしました';
+});
+
